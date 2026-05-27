0f0bb096799a5023889b6a1262be27af837fc043
elcorremachitos <elcorremachitos.2015@gmail.com>
2026-05-26 19:30:00 -0500

    first commit

---
+++ src/main/java/com/chamo/chamosports/ChamoSportsApplication.java
@@ -0,0 +1,13 @@
+package com.chamo.chamosports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChamoSportsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChamoSportsApplication.class, args);
+    }
+
+}
